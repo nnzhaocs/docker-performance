9ceed26fe6dd980b16eb95f334cdf44d92b2cbc8
nnzhaocs <znannan1@vt.edu>
2019-03-04 19:25:20 -0500

    fixing stat()

--- client.py
+++ client.py
@@ -130,7 +130,7 @@ def send_requests(wait, requests, startTime, q):
             while not onTime_q.empty():
             	onTime_l.extend(onTime_q.get())
                 
-            results.append({'time': now, 'duration': t, 'onTime': onTime_l})   #, 'size': size
+            results.append({'time': now, 'duration': t, 'onTime': onTime_l, 'size': r['size']})
 #             pull_rsp_q.put(results)   
         
             print 'processes joined, send requests continuing'
