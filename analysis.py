9f352d89e03f5c15e411c0577109a91f78360f34
nnzhaocs <znannan1@vt.edu>
2019-03-28 17:56:41 -0400

    calbatchstats

--- analysis.py
+++ analysis.py
@@ -534,14 +534,14 @@ def durationmanifestblobs():
 
 def numpy_fillna(data):
     # Get lengths of each row of data
-    lens = np.array([len(i) for i in data])
+    lens = numpy.array([len(i) for i in data])
 
     # Mask of valid places in each row
-    mask = np.arange(lens.max()) < lens[:,None]
+    mask = numpy.arange(lens.max()) < lens[:,None]
 
     # Setup output array and put elements from data into masked positions
-    out = np.zeros(mask.shape, dtype=data.dtype)
-    out[mask] = np.concatenate(data)
+    out = numpy.zeros(mask.shape, dtype=data.dtype)
+    out[mask] = numpy.concatenate(data)
     return out
 
 
