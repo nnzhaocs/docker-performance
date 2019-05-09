0ba27965e20b5f761d035f38df9f6830b1aeed72
nnzhaocs <znannan1@vt.edu>
2019-05-09 19:31:00 -0400

    update analysis

--- analysis.py
+++ analysis.py
@@ -513,7 +513,7 @@ def clusterClientReqs(total_trace):
 def clusterClientReqForClients(total_trace):
     organized = defaultdict(list)
     fname = os.path.basename(total_trace)
-    print "the output file would be: " + input_dir + fname + '-sorted_reqs_repo_clientForclients.lst'
+    print "the output file would be: " + input_dir + fname + '-sorted_reqs_repo_clientForclients.json'
     
     organized = clusterUserreqs(total_trace)     
      
@@ -560,110 +560,75 @@ def clusterClientReqForClients(total_trace):
         img_req_group[cli]= cli_img_req_group_new
 #     img_req_group.sort(key= lambda x: x[0]['delay'])
 #     return img_req_group
-    with open(input_dir + fname + '-sorted_reqs_repo_clientForclients.lst', 'w') as fp:
-        json.dump(img_req_group, fp)    
+    with open(input_dir + fname + '-sorted_reqs_repo_clientForclients.json', 'w') as fp:
+        json.dump(img_req_group, fp)
         
-#     fname = os.path.basename(total_trace) 
-#     print "the output file would be: " + input_dir + fname +'_repullRepoClient.json'
-#     with open(input_dir + fname + '-sorted_reqs_repo_client.lst', 'r') as fp:
-#         blob = json.load(fp) 
+        
+
+def clusterClientRepoPull(total_trace):    
+    fname = os.path.basename(total_trace) 
+    print "the output file would be: " + input_dir + fname +'_clusterClientRepoPull.json'
+    with open(input_dir + fname + '-sorted_reqs_repo_clientForclients.json', 'r') as fp:
+        blob = json.load(fp) 
+      
+    client_repo_dict = defaultdict(list)
     
-#     client_repo_dict = defaultdict(list)
-#          
-#     for r in blob:
-#         uri = r[0]['uri']
-# #         layer_or_manifest_id = uri.rsplit('/', 1)[1]
-#         parts = uri.split('/')
-#         repo = parts[1] + '/' + parts[2]
-#         #key = client address : reponame
-#         
-#         if (r[0]['method'] != 'GET') or ('manifest' not in r[0]['uri']):
-#             print "a wrong requests"
-#             print r
-#             continue
-#         
-#         key = r[0]['clientAddr'] + ':' + repo
-#         
-#         tup = (r[0]['delay'], len(r) - 1)
-#         print key
-#         print tup
-#         client_repo_dict[key].append(tup)
-#         
-# #         try:
-# #             lst = client_repo_dict[key]
-# #             client_repo_dict[key].append(r)
-# #         except Exception as e:
-# #             client_repo_dict[key].append(r)
-#     with open(input_dir + fname +'_getgetmanifests.json', 'w') as fp:
-#         json.dump(client_repo_dict, fp)
-#     
-#     GetM_l = [] # duration between two subsequent get manifest with layers for same client and same repo
-#     GetM_ln = [] # duration between two subsequent get manifests, first with layers, later without layers
-#     nothing = 0
-#     pulls  = 0       
-#     for key, lst in client_repo_dict.items():
-#     pulls += 1
-#         if (len(lst) < 2) and (0 == lst[0][1]):
-#             nothing += 1
-#             print "this client doesn't pull anything from this repo at all"
-#     if len(lst) < 2:
-#             continue
-#         
-#         first = False
-#         prev = 0
-#         for tup in lst:
-#             if tup[1] != 0:
-#                 if first: # prev get manifest with layers, and this also has layer
-#                     t = datetime.datetime.strptime(tup[0], '%Y-%m-%dT%H:%M:%S.%fZ')
-#                     delta = t - prev
-#                     delta = delta.total_seconds()
-#                     GetM_l.append(delta)
+    f = open(input_dir + fname + '-_clusterClientRepoPull.lst', 'w')
+    
+    for client, rlst in blob: 
+        repo_state_dict = {}
+        if 0 == len(rlst):
+            print "empty reqs"
+        
+        for r in rlst:
+            uri = r[0]['uri']
+#         layer_or_manifest_id = uri.rsplit('/', 1)[1]
+
+            if (r[0]['method'] != 'GET') or ('manifest' not in r[0]['uri']):
+                print "a wrong request"
+                print r
+                continue
+        
+            parts = uri.split('/')
+            repo = parts[1] + '/' + parts[2]
+            #key = client address : reponame
+            layer_or_manifest_id = uri.rsplit('/', 1)[1]
+            key = r[0]['clientAddr'] + ':' + repo   
+             
+            if 1 == len(r):
+                print "empty get manifest request"
+                if key in repo_state_dict.keys():
+                    repo_state_dict[key][0] += 1
+                    repo_state_dict[key][2] += 1
+                else:
+                    repo_state_dict[key] = (1,0,1,[]) # empty get count, repull cnt, total pull cnt
+            llst = []
+            for l in r[1:]:
+                lid = layer_or_manifest_id = uri.rsplit('/', 1)[1]
+                llst.append[lid]
+                
+            if key in repo_state_dict.keys():
+                prev_lst = repo_state_dict[key][3]
+                repulled_lst = list(set(prev_lst) & set(llst))
+                if len(repulled_lst) >= 0.5*len(prev_lst) or len(repulled_lst) >= 0.5*len(llst):
+                    repo_state_dict[key][1] += 1
 #                 else:
-#                     prev = datetime.datetime.strptime(tup[0], '%Y-%m-%dT%H:%M:%S.%fZ')
-#             first = True
-#             else:
-#                 if first: # prev get manifest layers, and this don't has layer
-#                     t = datetime.datetime.strptime(tup[0], '%Y-%m-%dT%H:%M:%S.%fZ')
-#                     delta = t - prev
-#                     delta = delta.total_seconds()
-#                     GetM_ln.append(delta)
-# #                 else: # prev dont have layers and so as this one
-# #                     prev = datetime.datetime.strptime(lst[0][0], '%Y-%m-%dT%H:%M:%S.%fZ') 
-# #                     pass
-# #         print rintervals_GET_ML
-# #         intervals_GET_MLs.append(rintervals_GET_ML)
-# #         lst.extend(rintervals_GET_ML)
-#     print "num of clients repo reqs do not pull anything: " + str(nothing*1.0/pulls) 
-#     print "avg interval between a get manifest with layers and a get manifest with layers:" + str(sum(GetM_l)*1.0 / len(GetM_l)) 
-#     print "number: "+str(len(GetM_l))
-#     print "midian is:  "+ str(statistics.median(GetM_l)) 
-# 
-#     print "all intervals:====>"
-#     print "mean: "+ str(numpy.mean(GetM_l))
-#     print "10 percentile: "+str(numpy.percentile(GetM_l, 10))
-#     print "25 percentile: "+str(numpy.percentile(GetM_l, 25))
-#     print "39 percentile: "+str(numpy.percentile(GetM_l, 39))
-#     print "max: "+str(numpy.amax(GetM_l))
-#     print "min: "+str(numpy.amin(GetM_l)) 
-#     
-#     print "avg interval between a get manifest with layers and a get manifest without layers:" + str(sum(GetM_ln)*1.0 / len(GetM_ln)) 
-#     print "number: "+str(len(GetM_ln))
-#     print "midian is:  "+ str(statistics.median(GetM_ln)) 
-#     
-#     print "all intervals:====>"
-#     print "mean: "+ str(numpy.mean(GetM_ln))
-#     print "1 percentile: "+str(numpy.percentile(GetM_ln, 1))
-#     print "25 percentile: "+str(numpy.percentile(GetM_ln, 25))
-#     print "39 percentile: "+str(numpy.percentile(GetM_ln, 39))
-#     print "max: "+str(numpy.amax(GetM_ln))
-#     print "min: "+str(numpy.amin(GetM_ln))
-# 
-# 
-#     with open(input_dir + fname +'_getgetmanifests_GetM_l.json', 'w') as fp:
-#         json.dump(GetM_l, fp)
-#         
-#     with open(input_dir + fname +'_getgetmanifests_GetM_ln.json', 'w') as fp:
-#         json.dump(GetM_ln, fp)  
+                repo_state_dict[key][2] += 1
+                
+                repo_state_dict[key][3] = list(set(prev_lst) | set(llst))
+            else:
+                repo_state_dict[key] = (0,0,1,llst) # empty get count, repull cnt, partial pull cnt
+        
+        client_repo_dict[client].append(repo_state_dict)
+             
+        print key
+        print repo_state_dict[key]
+        f.write(str(repo_state_dict[key][0])+'\t'+str(repo_state_dict[key][1])+'\t'+str(repo_state_dict[key][2])+'\t\n')
+         
+    with open(input_dir + fname +'_clusterClientRepoPull.json', 'w') as fp:
+        json.dump(client_repo_dict, fp)
+        
+    f.close()
 
 
 def repullReqsCal(total_trace):
@@ -1182,7 +1147,10 @@ def main():
         repullReqsCal(trace_dir)
     elif args.command == 'clusterClientReqForClients':
         clusterClientReqForClients(trace_dir)
+    elif args.command == 'clusterClientRepoPull':
+        clusterClientRepoPull(total_trace)
         return
+
 
 if __name__ == "__main__":
     main()
