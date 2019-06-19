39c0893fd29659630d73a732237271f16ecd0dee
nnzhaocs <znannan1@vt.edu>
2019-06-19 17:22:37 -0400

    run_orig.sh now supports dual mode

--- run/run_orginialtest.sh
+++ run/run_orginialtest.sh
@@ -6,10 +6,12 @@
 
 #rm -rf /home/nannan/dockerimages/docker-traces/data_centers/dal09/prod-dal09-logstash-2017.0*-realblob*
 ####:============ run originalregistrycluster======================######
-if [$1 == "run"]; then
+if [ $1 == "run" ]; then
+    echo "in run mode"
     pssh -h remotehostshulk.txt -l root -A -i 'docker run --rm -d -p 5000:5000 --mount type=bind,source=/home/nannan/testing/tmpfs,target=/var/lib/registry/docker/registry/v2/pull_tars/ -v=/home/nannan/testing/layers:/var/lib/registry -e "REGISTRY_STORAGE_CACHE_HOSTIP=$(ip -4 addr |grep 192.168 |grep -Po "inet \K[\d.]+")" --name originalregistry-3  nnzhaocs/distribution:original'
 ####:=========== gather docker logs together and extract results =====================
 else
+    echo "in info gathering mode"
     #create a new directory in amaranth with timestamp
     $dir = $(date +%Y%m%d_%H%M%S)
     mkdir -p /home/nannan/testing/resultslogs/$dir
