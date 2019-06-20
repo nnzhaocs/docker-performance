bb0e024a996e97b5e7ed41dc86204f480ee310ac
nnzhaocs <znannan1@vt.edu>
2019-06-20 10:24:01 -0400

    update

--- run/get_originaltest_results.sh
+++ run/get_originaltest_results.sh
@@ -7,14 +7,17 @@ awk '/metadata lookup time/{print}' logs > data
 awk -F'metadata lookup time:' '{print $2}' data > tmp1
 awk -F',' '{print $1}' tmp1 > metadatalookuptime
 ### cp time
-awk '/layer cp time/{print}' logs > data
+#awk '/layer cp time/{print}' logs > data
 awk -F'layer cp time:' '{print $2}' data > tmp1
 awk -F',' '{print $1}' tmp1 > layercpiotime
 ### transfer time
-awk '/layer transfer time/{print}' logs
+#awk '/layer transfer time/{print}' logs
 awk -F'layer transfer time:' '{print $2}' data > tmp1
 awk -F',' '{print $1}' tmp1 > layerpreparetransfertime
+### size
+awk -F'layer size:' '{print $2}' data > tmp1
+awk -F',' '{print $1}' tmp1 > layersize
 
-paste -d"\t" metadatalookuptime layercpiotime layerpreparetransfertime > registry_results.lst  
-rm data tmp1 logs metadatalookuptime layercpiotime layerpreparetransfertime
+paste -d"\t" metadatalookuptime layercpiotime layerpreparetransfertime layersize > registry_results.lst  
+rm data tmp1 logs metadatalookuptime layercpiotime layerpreparetransfertime layersize
 cp registry_results.lst ../
