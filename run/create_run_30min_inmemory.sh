3db429172224b0d35600fe71fb304e3e57f9af25
nnzhaocs <znannan1@vt.edu>
2019-09-14 16:44:41 -0400

    update

---
+++ run/create_run_30min_inmemory.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+#-----------
+# ./create_yaml.sh 
+# python create_yaml.py -t dal -m sift -s selective -a 8 -n 7 -c 4 -p 2
+# sshpass -p 'kevin123' pssh -h remotehostthors.txt -l root -A -t 400 -i "docker ps"
+#----------
+
+date
+
+echo "testing params:"
+#echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+echo $7
+echo $8
+
+codedir="/home/nannan/docker-performance"
+testingmachine=3
+cachesizeratio=0.25
+repullthres=3
+
+echo "cleanup thors..."
+./cleanup_thors.sh
+
+echo "sleep 10 s, wait for cleaning"
+sleep 10
+
+echo "create config, registry, and client file"
+python create_yaml.py -t $2 -m $3 -s $4 -a $5 -n $6 -c $7 -p $8
+
+echo "cp config.yaml file to other client machines"
+sshpass -p 'kevin123' pssh -h clients.txt  -l root -A -i "sshpass -p 'nannan' scp nannan@amaranth$testingmachine:$codedir/config.yaml  $codedir/"
+
+echo "sleep 10 s, wait for cp config.yml"
+sleep 10
+
+echo "kill all old pythons"
+sshpass -p 'kevin123' pssh -h clients.txt -l root -A -i -t 600 'pkill -9 python'
+
+# first run match
+cd $codedir
+
+echo "run match"
+python master.py -c match -i config.yaml
+
+# Second run containers
+# -----------------------------------------
+# nodedup:     nnzhaocs/distribution:original
+# primary: b-mode 3:  nnzhaocs/distribution:primary-b-mode3
+# restore:
+#  (1) nnzhaocs/distribution:distributionthors7
+#  (2) nnzhaocs/distribution:distributionthors14
+# b-mode 0: 
+# nnzhaocs/distribution:b-mode-0 (21 nodes)
+# sift: standard:
+# nnzhaocs/distribution:primary14 + nnzhaocs/distribution:original
+# nnzhaocs/distribution:primary14 + nnzhaocs/distribution:original
+# <shrink to 7> 
+# sift: selective:
+# same as standard
+# 
+# ------------------------------------------
+
+dedupimagearr=("nnzhaocs/distribution:distributionthors7" "nnzhaocs/distribution:distributionthors14" "nnzhaocs/distribution:distributionthors21")
+originalimage="nnzhaocs/distribution:original"
+#primaryimagearr=("nnzhaocs/distribution:primarythors7" "nnzhaocs/distribution:primarythors14" "nnzhaocs/distribution:primarythors21")
+inmemoryimage="nnzhaocs/distribution:inmemory"
+declare -A sizearr
+sizearr["dal"]=20
+sizearr+=(["dev"]=13 ["fra"]=6 ["lon"]=10 ["prestage"]=60 ["stage"]=7 ["syd"]=2)
+
+echo "start containers ......"
+cd $codedir"/run"
+
+printf "\n\nTESTMODE: %s\n\n" $3
+
+echo "first start dedup containers----------->"
+
+if [ $3 == "restore" ]; then
+	cachesize=$(echo "${sizearr[$2]}*1024*$cachesizeratio/$6+1"|bc)
+	echo "cachesize:"
+	echo $cachesize
+
+	imageno=$(echo "$6/7-1"|bc)
+	echo ${dedupimagearr[$imageno]}
+	./run_sifttest.sh ${dedupimagearr[$imageno]} dedupregistries.txt $cachesize $repullthres
+	echo "sleep 20 s, wait for dedup registries to start"
+	sleep 20
+	echo "save dedup registry containers' logs"
+	sshpass -p 'kevin123' pssh -h dedupregistries.txt -l root -A -i 'docker logs -f $(docker ps -a -q) &>> /home/nannan/logs-nondedup &'
+	sleep 20 
+	echo "sleep 20 s, wait for dedup registries to start"
+	./run_sifttest_restore.sh $originalimage dedupregistries.txt
+elif [ $3 == "sift" ]; then
+	./run_sifttest_original.sh $originalimage dedupregistries.txt
+fi;
+
+echo "sleep 20 s, wait for dedup registries to run a while if any"
+sleep 20
+
+echo "second start primary containers----------->"
+
+if [ $3 == "nodedup" ]; then
+	./run_sifttest_original.sh $originalimage primaryregistries.txt
+elif [ $3 == "sift" ]; then
+	./run_sifttest_primary.sh $originalimage primaryregistries.txt 
+elif [ $3 == "primary" ]; then
+	echo $inmemoryimage
+	cachesize=$(echo "${sizearr[$2]}*1024*$cachesizeratio/21+1"|bc)
+	echo "cachesize:"
+	echo $cachesize
+	newcachesize=$(echo "$cachesize"|bc)
+	echo $newcachesize
+
+	#./run_sifttest.sh ${primaryimagearr[2]} primaryregistries.txt $newcachesize $repullthres
+	./run_sifttest_inmemory.sh nnzhaocs/distribution:inmemoryonenode primaryregistries.txt $newcachesize $repullthres
+fi;
+
+echo "sleep 20 s, wait for primary registries to start"
+sleep 20
+
+echo "start clients .......\n"
+echo "warmup ....."
+./run_clients.sh config.yaml warmup $codedir clients.txt
+
+echo "sleep 20 s, wait for client to start sending warmup requests"
+sleep 20
+
+sshpass -p 'kevin123' pssh -h clients.txt -l root -A -i -t 600 "cd $codedir; tail -n 50 logs"
+
+if [ $3 == "restore" ]; then
+	echo "sleep 20 min, wait for warmup to finish"
+	sleep 1200
+else
+	echo "sleep 20 min, wait for warmup to finish"
+	sleep 1200
+fi;
+
+sshpass -p 'kevin123' pssh -h clients.txt -l root -A -i -t 600 "cd $codedir; tail -n 20 logs"
+
+echo "run ...."
+./run_clients.sh config.yaml run $codedir clients.txt
+
+echo "sleep 20 s, wait for client to start sending run requests"
+sleep 20
+sshpass -p 'kevin123' pssh -h clients.txt -l root -A -i -t 600 "cd $codedir; tail -n 50 logs"
+
+echo "sleep 40 min, wait for run to finish"
+sleep 2400
+
+sshpass -p 'kevin123' pssh -h clients.txt -l root -A -i -t 600 "cd $codedir; tail -n 20 logs"
+
+./get_results_fromclients.sh $testingmachine clients.txt
+
+echo "sleep 20 s, wait for getting clients results.json file"
+sleep 20
+./get_alllogs_thors.sh $testingmachine
---
+++ run/create_yaml_onenode.py
@@ -0,0 +1,268 @@
+import os
+import sys
+import string
+import yaml
+from argparse import ArgumentParser
+import time
+
+# default setting
+traces = {}
+realblobfiles = {}
+limitamount = 1000 #5000
+# 24 32 48 64 
+warmupthreads = 100 # number of total clients
+hotratio = 0.25
+#nondedupreplicas = 2
+replicalevel = 1
+wait = True
+
+dir = '/home/nannan/docker-performance/'
+layerfiledir = '/home/nannan/dockerimages/layers/hulk1'
+hulk1layerfiledir = '/home/nannan/dockerimages/layers'
+
+def createclientinfo(trace):
+    client_info = {
+        "threads": 1,
+        "realblobs": [os.path.join(layerfiledir, trace+'_layers.lst')],
+            
+            }
+    return client_info
+
+def createtrace(traces, limit):
+    trace = {
+        "location": "/home/nannan/dockerimages/docker-traces/data_centers",
+        "traces": traces,
+        "limit": {
+            "amount": limit,
+            },
+        "output": "results.json",
+        
+        }
+    return trace
+
+def createwarmup(threads):
+    warmup = {
+        "output": "warmup_output.json",
+        "threads": threads,
+        } 
+    return warmup
+    
+def createtestmode(testmode):
+    nodedup = False
+    sift = False
+    restore = False
+    primary = False
+    
+    if testmode == "nodedup":
+        #nodedup = True
+        primary = True
+    if testmode == "sift":
+        sift = True
+    if testmode == "restore":
+        restore = True
+    if testmode == "primary":
+        #nodedup = True
+        primary = True
+    
+    testmode = {
+        "nodedup": nodedup,
+        "sift": sift,
+        "restore": restore,
+        "primary": primary,
+        }
+    
+    return testmode
+
+def createsiftparams(mode, hotratio, nondedupreplicas):
+    siftparams = {
+        "mode": mode,
+        "selective":{
+            "hotratio": hotratio,            
+            },
+        "standard":{
+            "nondedupreplicas": nondedupreplicas,
+            },
+        }
+    return siftparams
+
+def createsimulate(wait, accelerater, replicalevel):
+    simulate = {
+        "wait": wait,
+        "accelerater": accelerater,
+        "replicalevel": replicalevel,
+        }
+    
+    return simulate
+
+"""
+dal_layers.lst  fra_layers.lst  prestage_layers.lst  syd_layers.lst
+dev_layers.lst  lon_layers.lst  stage_layers.lst     testing_layers.lst
+"""
+
+def main():
+    #first add traces
+    traces["dal"] = ["dal09/prod-dal09-logstash-2017.06.20-0.json"]
+    traces["dev"] = ["dev-mon01/dev-mon01-logstash-2017.07.13-0.json", 
+                     "dev-mon01/dev-mon01-logstash-2017.07.13-1.json",
+                     "dev-mon01/dev-mon01-logstash-2017.07.13-2.json",
+                     "dev-mon01/dev-mon01-logstash-2017.07.13-3.json",
+                     "dev-mon01/dev-mon01-logstash-2017.07.14-0.json",
+                     "dev-mon01/dev-mon01-logstash-2017.07.14-1.json",
+                     "dev-mon01/dev-mon01-logstash-2017.07.14-2.json",
+                     "dev-mon01/dev-mon01-logstash-2017.07.14-3.json",
+                     "dev-mon01/dev-mon01-logstash-2017.07.15-0.json",
+                     "dev-mon01/dev-mon01-logstash-2017.07.15-1.json"]
+    traces["fra"] = ["fra02/prod-fra02-logstash-2017.06.20-0.json",
+                     "fra02/prod-fra02-logstash-2017.06.20-1.json",
+                     "fra02/prod-fra02-logstash-2017.06.20-3.json",
+                     "fra02/prod-fra02-logstash-2017.06.21-0.json",
+                     "fra02/prod-fra02-logstash-2017.06.21-1.json",
+                     "fra02/prod-fra02-logstash-2017.06.21-2.json",
+                     "fra02/prod-fra02-logstash-2017.06.21-3.json",
+                     "fra02/prod-fra02-logstash-2017.06.22-0.json",
+                     "fra02/prod-fra02-logstash-2017.06.22-1.json",
+                     "fra02/prod-fra02-logstash-2017.06.22-2.json",
+                     "fra02/prod-fra02-logstash-2017.06.22-3.json"]
+    traces["lon"] = ["lon02/prod-lon02-logstash-2017.06.20-0.json"]
+    traces["prestage"] = ["prestage-mon01/prestage-mon01-logstash-2017.07.03-0.json",
+                          "prestage-mon01/prestage-mon01-logstash-2017.07.03-1.json",
+                          "prestage-mon01/prestage-mon01-logstash-2017.07.03-2.json",
+                          "prestage-mon01/prestage-mon01-logstash-2017.07.03-3.json",
+                          "prestage-mon01/prestage-mon01-logstash-2017.07.04-0.json",
+                          "prestage-mon01/prestage-mon01-logstash-2017.07.04-1.json",
+                          "prestage-mon01/prestage-mon01-logstash-2017.07.04-2.json",
+                          "prestage-mon01/prestage-mon01-logstash-2017.07.04-3.json"]
+    traces["stage"] = ["stage-dal09/stage-dal09-logstash-2017.06.27-0.json"]
+    traces["syd"] = ["syd01/prod-syd01-logstash-2017.07.01-0.json",
+                     "syd01/prod-syd01-logstash-2017.07.01-1.json",
+                     "syd01/prod-syd01-logstash-2017.07.01-2.json",
+                     "syd01/prod-syd01-logstash-2017.07.01-3.json",
+                     "syd01/prod-syd01-logstash-2017.07.02-0.json",
+                     "syd01/prod-syd01-logstash-2017.07.02-1.json",
+                     "syd01/prod-syd01-logstash-2017.07.02-2.json",
+                     "syd01/prod-syd01-logstash-2017.07.02-3.json"]
+    
+    registries=["192.168.0.200:5000",
+                #"192.168.0.201:5000",
+                #"192.168.0.202:5000",
+                #"192.168.0.203:5000",
+                #"192.168.0.204:5000",
+                #"192.168.0.205:5000",
+                #"192.168.0.208:5000",
+                #"192.168.0.209:5000",
+                #"192.168.0.210:5000",
+                #"192.168.0.211:5000",
+                #"192.168.0.212:5000",
+                #"192.168.0.213:5000",
+                #"192.168.0.214:5000",
+                #"192.168.0.215:5000",
+                #"192.168.0.216:5000",
+                #"192.168.0.217:5000",
+                #"192.168.0.218:5000",
+                #"192.168.0.219:5000",
+                #"192.168.0.221:5000",
+                #"192.168.0.222:5000",
+                #"192.168.0.223:5000"
+                ]
+    
+    clients_amaranths = ["192.168.0.151",
+               "192.168.0.153",
+               "192.168.0.154",
+               "192.168.0.156"]
+
+    clients = ["192.168.0.220"]
+
+    clients_hulks = ["192.168.0.170",
+            "192.168.0.171",
+            "192.168.0.172",
+            "192.168.0.174",
+           "192.168.0.176",
+           "192.168.0.177",
+           "192.168.0.179",
+           "192.168.0.180"]
+
+    
+    
+    parser = ArgumentParser(description='Trace Player, allows for anonymized traces to be replayed to a registry, or for caching and prefecting simulations.')
+#     parser.add_argument('-r', '--realblobfiles', dest='realblobfiles', type=str, required=True, 
+#                         help = 'input realblob files: 50m or 1gb')
+    parser.add_argument('-t', '--tracefiles', dest='tracefiles', type=str, required=True, 
+                        help = 'input trace file: dal, dev, fra, prestage, or syd, lon')
+    parser.add_argument('-m', '--testmode', dest='testmode', type=str, required=True, 
+                        help = 'input test mode: nodedup, sift, restore')
+    parser.add_argument('-s', '--siftmode', dest='siftmode', type=str, required=True, 
+                        help = 'input sift mode: standard, selective')
+    parser.add_argument('-a', '--accelerater', dest='accelerater', type=int, required=True, 
+                        help = 'input accelerater: int')
+    parser.add_argument('-n', '--numofdedupregistries', dest='numofdedupregistries', type=int, required=True, 
+                        help = 'input numofdedupregistries: int')
+    parser.add_argument('-c', '--numofclients', dest='numofclients', type=int, required=True, 
+                        help = 'input numofclients: int')
+    parser.add_argument('-p', '--nondedupreplicas', dest='nondedupreplicas', type=int, required=True,
+                                    help = 'input nondedupreplicas: int')
+
+    
+    args = parser.parse_args()
+    print args
+    client_info = createclientinfo(args.tracefiles)
+    testingtrace = createtrace(traces[args.tracefiles], limitamount)
+
+    primaryregistry=[]
+    dedupregistry=[]
+
+    testingclients = clients[:args.numofclients]
+    warmup = createwarmup(warmupthreads)
+    """
+    nodedup: original registry;
+    primary: b-mode 3
+    restore: b-mode 0
+    sift:
+    standard: b-mode 2 or b-mode 1
+    selective: 
+    """
+    if args.testmode == "nodedup" or args.testmode == "primary":
+        primaryregistry = registries #registries(:,len(registries)-args.numofdedupregistries)
+    elif args.testmode == "sift":
+        dedupregistry = registries[:args.numofdedupregistries]
+        primaryregistry = registries[-(len(registries)-args.numofdedupregistries):]
+    elif args.testmode == "restore":
+        dedupregistry = registries[:args.numofdedupregistries]
+#    elif args.testmode == "primary":
+#        dedupregistry = registries[:args.numofdedupregistries]
+#        primaryregistry = registries[-(len(registries)-args.numofdedupregistries):]
+        
+    testingmode = createtestmode(args.testmode)
+    testingsiftmode = createsiftparams(args.siftmode, hotratio, args.nondedupreplicas)
+    simulate = createsimulate(wait, args.accelerater, replicalevel)
+    config = {
+        "client_info": client_info,
+        "trace": testingtrace,
+        "primaryregistry": primaryregistry,
+        "dedupregistry": dedupregistry,
+        "clients": testingclients,
+        "warmup": warmup,
+        "testmode": testingmode,
+        "siftparams": testingsiftmode,
+        "simulate": simulate,
+        }
+    print config
+    with open(os.path.join(dir, "config.yaml"), 'w') as fp:
+        yaml.dump(config, fp, default_flow_style=False)
+    with open(os.path.join(dir, "run/clients.txt"), 'w') as fp:
+        for i in testingclients:
+            fp.write(i+':22\n')
+    with open(os.path.join(dir, "run/dedupregistries.txt"), 'w') as fp:
+        for i in dedupregistry:
+            tmp = i.split(':')[0]
+            fp.write(tmp+':22\n')
+    with open(os.path.join(dir, "run/primaryregistries.txt"), 'w') as fp:
+        for i in primaryregistry:
+            tmp = i.split(':')[0]
+            fp.write(tmp+':22\n')
+
+            
+    
+    
+    
+if __name__ == "__main__":
+        main()
